0a219e365d9b465d955e97f9228373f8c5aa8796
Edward Freier <edwardfreier01@gmail.com>
2026-09-16 19:25:07 -0500

    Add files via upload

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using ContactApp.Models;
+using ContactApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactApp.Pages
+{
+    public class ContactModel : PageModel
+    {
+        //create form variables
+        private readonly ContactStore _store;
+
+        [BindProperty]
+        public Contact Form { get; set; }
+        public ContactModel(ContactStore store) => _store = store;
+
+
+        //form logic to load
+        public void OnGet() { /*display empty form */ }
+
+
+        //form logic to post
+        public IActionResult OnPost()
+        {
+            //validate model
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+
+            //add submission to store
+            Form.SubmittedUtc = DateTime.UtcNow;
+            _store.Add(Form);
+
+            //redirect to thank you page
+            return RedirectToPage("/ThankYou");
+        }
+    }
+}
---
+++ Services/ContactStore.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Json;
+using ContactApp.Models;
+
+
+namespace ContactApp.Services
+{
+    public class ContactStore
+    {
+        //Variables to handle I/O
+        private readonly string _dataDir;
+        private readonly string _dataFile;
+        private readonly object _lock = new();
+        private readonly JsonSerializerOptions _json = new() { WriteIndented = true };
+
+        //List of Contacts to hold form submissions in RAM
+        private List<Contact> _cache = new();
+
+
+        //Constructor to handle working in the web environment
+        public ContactStore(IWebHostEnvironment env)
+        {
+            //set the pathing
+            _dataDir = Path.Combine(env.ContentRootPath, "data");
+            _dataFile = Path.Combine(_dataDir, "contacts.json");
+
+
+            //create the directory if it doesn't exist
+            Directory.CreateDirectory(_dataDir);
+
+            //Load all form responses
+            LoadFromDisk();
+        }
+
+
+        //method to get all form submissions
+        public IReadOnlyList<Contact> GetAll()
+        {
+            lock (_lock) return _cache.ToList();
+        }
+
+        //void to add a contact
+        public void Add(Contact c) //prevent readers/writers problem
+        {
+            lock ( _lock)
+            {
+                _cache.Add(c); //Adds the contact to the list of contacts
+                SaveToDisk(); //Immedietly saves the Json File
+            }
+        }
+
+        //void to load from the disk 
+        private void LoadFromDisk()
+        {
+            //Handle if the Json does not exist
+            if (!File.Exists(_dataFile)) { _cache = new(); return; }
+
+            try
+            {
+                var text = File.ReadAllText(_dataFile);
+                _cache = JsonSerializer.Deserialize<List<Contact>>(text) ?? new();
+            }
+            catch
+            {
+                _cache = new(); //if corrupted, start fresh
+            }
+        }
+        
+
+        //void to save to disk
+        private void SaveToDisk()
+        {
+            var text = JsonSerializer.Serialize(_cache, _json);
+            File.WriteAllText(_dataFile, text);
+        }
+    }
+}
